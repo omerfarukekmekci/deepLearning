63079ca45fe988f70f789343d43619228b8f7fca
itu-itis-ekmekci23 <omerfarukekmekci2004@gmail.com>
2025-08-26 17:00:58 +0300

    split the code into functions and generalized it

--- nn.py
+++ nn.py
@@ -12,90 +12,105 @@ def tanh(Z):
     return np.tanh(Z)
 
 
-# loading the dataset
-train_dataset = h5py.File("cats_vs_dogs_96.h5", "r")
-X_train = np.array(train_dataset["X_train"][:])
-Y_train = np.array(train_dataset["Y_train"][:])
-X_test = np.array(train_dataset["X_test"][:])
-Y_test = np.array(train_dataset["Y_test"][:])
+def initialiseParameters(layer_dims):
+    L = len(layer_dims)
+    parameters = {}
+
+    for l in range(1, L):
+        parameters["W" + str(l)] = (
+            np.random.randn(layer_dims[l], layer_dims[l - 1]) * 0.01
+        )
+        parameters["b" + str(l)] = np.zeros((layer_dims[l], 1))
+
+    return parameters
 
 
-# reshaping the matrices
-Y_train = Y_train.reshape(1, -1)
-Y_test = Y_test.reshape(1, -1)
-X_train = X_train.reshape(X_train.shape[0], -1).T
-X_test = X_test.reshape(X_test.shape[0], -1).T
-m = X_train.shape[1]
+def linearForward(parameters, A0):
+    A = A0
+    caches = []
+    L = len(parameters) // 2
+
+    for l in range(1, L):
+        A_prev = A
+        Z = np.dot(parameters["W" + str(l)], A_prev) + parameters["b" + str(l)]
+        linear_cache = (A_prev, parameters["W" + str(l)], parameters["b" + str(l)])
+
+        A = tanh(Z)
+        activation_cache = Z
+
+        cache = (linear_cache, activation_cache)
+        caches.append(cache)
+
+    A_prev = A
+    Z = np.dot(parameters["W" + str(L)], A_prev) + parameters["b" + str(L)]
+    linear_cache = (A_prev, parameters["W" + str(L)], parameters["b" + str(L)])
+
+    A = sigmoid(Z)
+    activation_cache = Z
+
+    cache = (linear_cache, activation_cache)
+    caches.append(cache)
+
+    return A, caches
 
 
-# checking the dimensions
-print("X_train shape:", X_train.shape)
-print("Y_train shape:", Y_train.shape)
-print("X_test shape:", X_test.shape)
-print("Y_test shape:", Y_test.shape)
+def compute_cost(A, Y_train):
+    m = Y_train.shape[1]
+    cost = -np.sum(Y_train * np.log(A) + (1 - Y_train) * np.log(1 - A)) / m
+    cost = np.squeeze(cost)  # this makes the value a scalar instead of a 1*1 matrix
+
+    return cost
 
 
-# forward propagation
-layer_dims = [X_train.shape[1], 10, 3, 1]
-A0 = X_train
-caches = []
+def calculateDerivatives(A, Y_train, caches, parameters):
+    grads = {}
+    L = len(caches)
+    m = Y_train.shape[1]
 
-W1 = np.random.randn(layer_dims[1], A0.shape[0]) * 0.01
-b1 = np.zeros((layer_dims[1], 1))
-Z1 = np.dot(W1, A0) + b1
-A1 = tanh(Z1)
-linear_cache_1 = (A0, W1, b1)
-activation_cache_1 = Z1
-cache_1 = (linear_cache_1, activation_cache_1)
-caches.append(cache_1)
+    linear_cache, activation_cache = caches[L]
+    A_prev, W, b = linear_cache
+    Z = activation_cache
 
-W2 = np.random.randn(layer_dims[2], A1.shape[0]) * 0.01
-b2 = np.zeros((layer_dims[2], 1))
-Z2 = np.dot(W2, A1) + b2
-A2 = tanh(Z2)
-linear_cache_2 = (A1, W2, b2)
-activation_cache_2 = Z2
-cache_2 = (linear_cache_2, activation_cache_2)
-caches.append(cache_2)
+    dZ = A - Y_train
+    dW = np.dot(dZ, A_prev.T) / m
+    db = np.sum(dZ, axis=1, keepdims=True) / m
+    dA_prev = np.dot(W.T, dZ)
 
-W3 = np.random.randn(layer_dims[3], A2.shape[0]) * 0.01
-b3 = np.zeros((layer_dims[3], 1))
-Z3 = np.dot(W3, A2) + b3
-A3 = sigmoid(Z3)
-linear_cache_3 = (A2, W3, b3)
-activation_cache_3 = Z3
-cache_3 = (linear_cache_3, activation_cache_3)
-caches.append(cache_3)
+    grads["dW" + str(L)] = dW
+    grads["db" + str(L)] = db
 
+    for l in reversed(range(L - 1)):
+        linear_cache, activation_cache = caches[l]
+        A_prev, W, b = linear_cache
+        Z = activation_cache
 
-# calculate the cost
-cost = -np.sum(Y_train * np.log(A3) + (1 - Y_train) * np.log(1 - A3)) / m
-cost = np.squeeze(cost)  # this makes the value a scalar instead of a 1*1 matrix
+        dZ = dA_prev * (1 - np.tanh(Z) ** 2)
+        dW = np.dot(dZ, A_prev.T) / m
+        db = np.sum(dZ, axis=1, keepdims=True) / m
+        dA_prev = np.dot(W.T, dZ)
+
+        grads["dW" + str(l + 1)] = dW
+        grads["db" + str(l + 1)] = db
+
+    return grads
 
 
-# calculate the derivatives for the backwards propagation
-dZ3 = A3 - Y_train
-dW3 = np.dot(dZ3, A2.T) / m
-db3 = np.sum(dZ3, axis=1, keepdims=True) / m
-dA2 = np.dot(W3.T, dZ3)
+def updateParameters(parameters, grads, learning_rate=0.01):
+    L = len(parameters) // 2
 
-dZ2 = dA2 * (1 - A2**2)
-dW2 = np.dot(dZ2, A1.T) / m
-db2 = np.sum(dZ2, axis=1, keepdims=True) / m
-dA1 = np.dot(W2.T, dZ2)
+    for l in range(1, L):
+        parameters["W" + str(l)] -= learning_rate * grads["dW" + str(l)]
+        parameters["b" + str(l)] -= learning_rate * grads["db" + str(l)]
 
-dZ1 = dA1 * (1 - A1**2)
-dW1 = np.dot(dZ1, A0.T) / m
-db1 = np.sum(dZ1, axis=1, keepdims=True) / m
+    return parameters
 
 
-# update parameters
-learning_rate = 0.01
+def train(layer_dims, numberOfGenerations=10):
+    parameters = initialiseParameters()
 
-for i in range(0, 100):
-    W1 -= learning_rate * dW1
-    b1 -= learning_rate * db1
-    W2 -= learning_rate * dW2
-    b2 -= learning_rate * db2
-    W3 -= learning_rate * dW3
-    b3 -= learning_rate * db3
+    for i in range(1, numberOfGenerations):
+        A, caches = linearForward(parameters, A0)
+        grads = calculateDerivatives(A, Y_train, caches, parameters)
+        parameters = updateParameters(parameters, grads, learning_rate=0.01)
+
+    return XXX
