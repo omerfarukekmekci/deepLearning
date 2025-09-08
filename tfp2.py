64371ac417d6c8e1ad5e374e19a8b92b13c55ef2
itu-itis-ekmekci23 <omerfarukekmekci2004@gmail.com>
2025-09-08 12:51:08 +0300

    final state of the fashion mnist seperator

--- tfp2.py
+++ tfp2.py
@@ -1,40 +1,53 @@
 import os
 
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"  # hide TensorFlow startup INFO
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"  # to hide tensorflow startup info
 
-import numpy as np
 import tensorflow as tf
 
 
 class myCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
-        if logs["loss"] < 0.2:
-            print("Loss is low so cancelling training!")
+        if logs.get("accuracy") and logs["accuracy"] > 0.95:
+            print("\nAccuracy is high enough, cancelling training.")
             self.model.stop_training = True
 
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-# Normalize the data
+# normalization
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
+train_images = train_images.reshape(-1, 28, 28, 1)
+test_images = test_images.reshape(-1, 28, 28, 1)
 
-model = tf.keras.Sequential(
-    [
-        tf.keras.Input(shape=(28, 28)),  # input shape must be in parentheses
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation=tf.nn.relu),
-        tf.keras.layers.Dense(10, activation=tf.nn.softmax),
-    ]
+
+def create_fashion_model():
+    model = tf.keras.Sequential(
+        [
+            tf.keras.Input(shape=(28, 28, 1)),
+            tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
+            tf.keras.layers.MaxPooling2D(2, 2),
+            tf.keras.layers.Conv2D(64, (2, 2), activation="relu"),
+            tf.keras.layers.MaxPooling2D(2, 2),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dense(10, activation="softmax"),  # 10 categories
+        ]
+    )
+    return model
+
+
+fashion_model = create_fashion_model()
+fashion_model.compile(
+    optimizer=tf.keras.optimizers.RMSprop(learning_rate=1e-4),
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy"],
 )
 
-# Compile the model (choose optimizer + loss function)
-model.compile(optimizer=tf.optimizers.Adam(), loss="sparse_categorical_crossentropy")
+fashion_model.fit(train_images, train_labels, epochs=10, callbacks=[myCallback()])
 
-model.fit(train_images, train_labels, epochs=5, callbacks=[myCallback()])
-
-
-# Make a prediction
-# print(model.predict(test_images))
+# evaluation
+test_loss, test_acc = fashion_model.evaluate(test_images, test_labels)
+print(f"Fashion-MNIST Test accuracy: {test_acc:.4f}")
